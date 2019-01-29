fab586ca6a557de87bd26bedfee3ebfba099bc5b
Andreas Blaesius <andi@unlegacy-android.org>
2019-01-29 14:00:56 +0100

    TWRP: fix compiling on Android 7
    fixes
    cp: cannot create regular file (...)/recovery/root/file_contexts': No such file or directory
    
    Change-Id: I094a0bd242c6eb01e84570fbe49e853098e33a8b

--- Android.mk
+++ Android.mk
@@ -499,10 +499,10 @@ endif
 endif
 
 # Only add file_contexts for Android versions N and O
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -le 27; echo $$?),0)
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 25; echo $$?),0)
-    LOCAL_REQUIRED_MODULES += file_contexts_text
-endif
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26; echo $$?),0)
+	LOCAL_REQUIRED_MODULES += file_contexts_text
+else ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 25; echo $$?),0)
+	LOCAL_ADDITIONAL_DEPENDENCIES += file_contexts_text
 endif
 
 ifeq ($(BOARD_CACHEIMAGE_PARTITION_SIZE),)
